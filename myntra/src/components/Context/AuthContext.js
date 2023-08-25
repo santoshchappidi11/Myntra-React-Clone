1511f7784025baf56d63d59d793e1c445f3ddd18
santoshchappidi11 <santoshchappidi3@gmail.com>
2023-08-25 15:50:27 +0530

    updates

--- myntra/src/components/Context/AuthContext.js
+++ myntra/src/components/Context/AuthContext.js
@@ -42,10 +42,10 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const getCurrentUser = async () => {
-      const token = JSON.parse(localStorage.getItem("MyntraUserToken")) || "";
+      const token = JSON.parse(localStorage.getItem("MyntraUserToken"));
       if (token?.length) {
         const response = await axios.post(
-          "http://localhost:8002/get-current-user",
+          "http://localhost:8001/get-current-user",
           { token }
         );
 
